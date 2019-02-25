832af189225b89e128356e576cf8252ea80ddee0
liuxyCN <lxy@live.cn>
2019-02-25 22:10:33 +0800

    turn to just plain object  Please enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 'use strict';
 
 const path = require('path');
-const Enum = require('./lib/enum');
 
 module.exports = app => {
-  app.Enum = Enum;
-
   const directory = path.join(app.config.baseDir, 'app/enums');
   app.loader.loadToApp(directory, 'enums', {
     inject: app,
